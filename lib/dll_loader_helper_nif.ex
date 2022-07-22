af5eed82946f369a4ad8990be55617338011de21
Cocoa <r.xu.1@research.gla.ac.uk>
2022-07-22 18:36:30 +0100

    bring back dll_loader_helper_nif.ex (#7)

---
+++ lib/dll_loader_helper_nif.ex
@@ -0,0 +1,25 @@
+defmodule :dll_loader_helper do
+  @moduledoc false
+
+  @on_load :load_nif
+  def load_nif do
+    case :os.type() do
+      {:win32, _} ->
+        nif_file = '#{:code.priv_dir(:dll_loader_helper)}/dll_loader_helper'
+
+        case :erlang.load_nif(nif_file, 0) do
+          :ok -> :ok
+          {:error, {:reload, _}} -> :ok
+          {:error, reason} -> {:error, reason}
+        end
+
+      _ ->
+        :ok
+    end
+  end
+
+  @spec add_dll_directory(binary()) :: :ok | {:error, String.t()}
+  def add_dll_directory(dir) when is_binary(dir) do
+    :ok
+  end
+end
