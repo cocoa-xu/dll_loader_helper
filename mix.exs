dbee1d0909276b590a454641f49df6999f1064fc
Cocoa <i@uwucocoa.moe>
2022-12-11 17:57:58 +0000

    added support for precompilation (#8)
    * added support for precompilation

--- mix.exs
+++ mix.exs
@@ -1,74 +1,36 @@
-defmodule Mix.Tasks.Compile.DllLoaderHelper do
-  use Mix.Task
-
-  @windows_error_msg ~S"""
-  One option is to install a recent version of
-  [Visual C++ Build Tools](https://visualstudio.microsoft.com/visual-cpp-build-tools/)
-  either manually or using [Chocolatey](https://chocolatey.org/) -
-  `choco install VisualCppBuildTools`.
-  After installing Visual C++ Build Tools, look in the "Program Files (x86)"
-  directory and search for "Microsoft Visual Studio". Note down the full path
-  of the folder with the highest version number. Open the "run" command and
-  type in the following command (make sure that the path and version number
-  are correct):
-      cmd /K "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-  This should open up a command prompt with the necessary environment variables
-  set, and from which you will be able to run the "mix compile", "mix deps.compile",
-  and "mix test" commands.
-  Another option is to install the Linux compatiblity tools from [MSYS2](https://www.msys2.org/).
-  After installation start the msys64 bit terminal from the start menu and install the
-  C/C++ compiler toolchain. E.g.:
-    pacman -S --noconfirm pacman-mirrors pkg-config
-    pacman -S --noconfirm --needed base-devel autoconf automake make libtool git \
-      mingw-w64-x86_64-toolchain mingw-w64-x86_64-openssl mingw-w64-x86_64-libtool
-  This will give you a compilation suite nearly compatible with Unix' standard tools.
-  """
-  def run(_) do
-    case :os.type() do
-      {:win32, _} ->
-        root_dir = :code.root_dir()
-        erts_dir = Path.join(root_dir, "erts-#{:erlang.system_info(:version)}")
-        erts_include_dir = System.get_env("ERTS_INCLUDE_DIR", Path.join(erts_dir, "include"))
-        System.put_env("ERTS_INCLUDE_DIR", erts_include_dir)
-        System.put_env("MIX_APP_PATH", Mix.Project.app_path())
-
-        opts = [
-          into: IO.stream(:stdio, :line),
-          stderr_to_stdout: true,
-          cd: Path.expand(File.cwd!()),
-        ]
-
-        Mix.Project.ensure_structure()
-        {%IO.Stream{}, status} = System.cmd("nmake", ["/F", "Makefile.win"], opts)
-        Mix.Project.ensure_structure()
-
-        case status do
-          0 -> :ok
-          _ ->
-            Mix.raise(~s{Could not compile with nmake" (exit status: #{status}).\n} <> @windows_error_msg)
-        end
-
-      _ -> :ok
-    end
-  end
-end
-
 defmodule DllLoaderHelper.MixProject do
   use Mix.Project
 
   @app :dll_loader_helper
   @github_url "https://github.com/cocoa-xu/dll_loader_helper"
+  @version "0.1.8"
   def project do
     [
       app: @app,
-      version: "0.1.8",
+      version: @version,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      compilers: [@app] ++ Mix.compilers(),
+      compilers: [:elixir_make] ++ Mix.compilers(),
       source_url: @github_url,
       description: description(),
       package: package(),
-      deps: deps()
+      deps: deps(),
+
+      # precompilation support
+      make_precompiler: {:nif, CCPrecompiler},
+      make_precompiler_filename: "dll_loader_helper",
+      make_precompiler_url: "#{@github_url}/releases/download/v#{@version}/@{artefact_filename}",
+
+      # precompiler configuration
+      cc_precompiler: [
+        only_listed_targets: true,
+        compilers: %{
+          {:win32, :nt} => %{
+            "x86_64-windows-msvc" => {"cl", "cl"},
+            "aarch64-windows-msvc" => {"cl", "cl"},
+          }
+        }
+      ]
     ]
   end
 
@@ -78,6 +40,8 @@ defmodule DllLoaderHelper.MixProject do
 
   defp deps do
     [
+      {:cc_precompiler, "~> 0.1", runtime: false, github: "cocoa-xu/cc_precompiler"},
+      {:elixir_make, ">= 0.0.0", github: "elixir-lang/elixir_make", override: true},
       {:ex_doc, "~> 0.28", only: :docs, runtime: false}
     ]
   end
@@ -90,8 +54,7 @@ defmodule DllLoaderHelper.MixProject do
     [
       name: to_string(@app),
       # These are the default files included in the package
-      files:
-        ~w(c_src CMakeLists.txt Makefile.win
+      files: ~w(c_src CMakeLists.txt Makefile.win
            mix.exs lib src rebar.config .formatter.exs
            README* LICENSE*),
       licenses: ["Apache-2.0"],
