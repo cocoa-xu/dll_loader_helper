ae9badb63bd64b1fb899668e82dcee1f9f848def
Cocoa <i@uwucocoa.moe>
2023-07-30 12:55:23 +0100

    fix init/0

--- dll_loader_helper_beam/src/dll_loader_helper_beam_nif.erl
+++ dll_loader_helper_beam/src/dll_loader_helper_beam_nif.erl
@@ -11,18 +11,23 @@
 -define(LIBNAME, dll_loader_helper_beam).
 
 init() ->
-    SoName = case code:priv_dir(?APPNAME) of
-        {error, bad_name} ->
-            case filelib:is_dir(filename:join(["..", priv])) of
-                true ->
-                    filename:join(["..", priv, ?LIBNAME]);
-                _ ->
-                    filename:join([priv, ?LIBNAME])
-            end;
-        Dir ->
-            filename:join(Dir, ?LIBNAME)
-    end,
-    erlang:load_nif(SoName, 0).
+    case os:type() of
+        {win32, nt} ->
+            SoName = case code:priv_dir(?APPNAME) of
+            {error, bad_name} ->
+                case filelib:is_dir(filename:join(["..", priv])) of
+                    true ->
+                        filename:join(["..", priv, ?LIBNAME]);
+                    _ ->
+                        filename:join([priv, ?LIBNAME])
+                end;
+            Dir ->
+                filename:join(Dir, ?LIBNAME)
+        end,
+        erlang:load_nif(SoName, 0);
+        _ ->
+            ok
+    end.
 
 not_loaded(Line) ->
     erlang:nif_error({not_loaded, [{module, ?MODULE}, {line, Line}]}).
